1cc1a4a2e30d2e3a2e5f55d8616bec9d97b745f8
Benjamin Horsleben <benjamin@fizkerinc.dk>
2021-08-17 13:28:32 +0200

    Updated UI of the UIKit app to be a lot more production-friendly

--- UIKitDemo/UIKitDemo/ViewController.swift
+++ UIKitDemo/UIKitDemo/ViewController.swift
@@ -1,6 +1,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+	@IBOutlet var latestUniversalLinkTextField: UITextField!
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
